673097b3fe31f7a11de243c745205f89b2470efb
azure <alan.lee.azure@gmail.com>
2016-06-11 17:30:27 +0800

    update.

--- cmp_keras.py
+++ cmp_keras.py
@@ -11,25 +11,25 @@ if __name__ == '__main__':
   X = np.random.rand(10, 5)
   y = (X.sum(axis=1)**3 + 1)[:,np.newaxis]
 
-  model2 = net.Net([net.Dense(5,1), net.Linear()])
+  model = net.Net([net.Dense(5,1), net.Linear()])
 
-  W = model2.layers[0].W.copy()
-  b = model2.layers[0].b.copy()
+  W = model.layers[0].W.copy()
+  b = model.layers[0].b.copy()
 
-  model2.fit(X, y, loss=util.MSE(), optimizer=util.SGD(0.1, 0), n_epoch=10, batch_size=10)
-  y_hat2 = model2.predict(X)
+  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(0.1, 0), n_epoch=10, batch_size=10)
+  y_hat = model.predict(X)
 
-  model = Sequential([
+  model1 = Sequential([
       Dense(1, input_dim=5),  #, weights=[W.T, b]),
       Activation('linear')
     ])
   sgd = SGD(lr=0.1, decay=0, momentum=0, nesterov=False)
-  model.compile(loss='mse', optimizer=sgd)
+  model1.compile(loss='mse', optimizer=sgd)
 
-  model.fit(X, y, nb_epoch=10, batch_size=10)
-  y_hat = model.predict(X)
+  model1.fit(X, y, nb_epoch=10, batch_size=10)
+  y_hat1 = model1.predict(X)
 
   pl.plot(y, '-^')
   pl.plot(y_hat, '--o')
-  pl.plot(y_hat2, '--*')
+  pl.plot(y_hat1, '--*')
   pl.show()
