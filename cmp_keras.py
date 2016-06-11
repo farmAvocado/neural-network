b38592f3b7a60af626d46b88c08a10df05634a91
azure <alan.lee.azure@gmail.com>
2016-06-11 17:50:02 +0800

    tested sigmoid.

--- cmp_keras.py
+++ cmp_keras.py
@@ -8,25 +8,25 @@ import net, util
 
 if __name__ == '__main__':
   np.random.seed(1)
-  X = np.random.rand(10, 5)
-  y = (X.sum(axis=1)**3 + 1)[:,np.newaxis]
+  X = np.random.rand(80, 5)
+  y = np.sin(X.sum(axis=1)**3 + 1)[:,np.newaxis]
 
-  model = net.Net([net.Dense(5,1), net.Linear()])
+  model = net.Net([net.Dense(5,1), net.Sigmoid()])
 
   W = model.layers[0].W.copy()
   b = model.layers[0].b.copy()
 
-  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(0.1, 0), n_epoch=10, batch_size=10)
+  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(1.1, 0), n_epoch=150, batch_size=10)
   y_hat = model.predict(X)
 
   model1 = Sequential([
       Dense(1, input_dim=5, weights=[W.T, b]),
-      Activation('linear')
+      Activation('sigmoid')
     ])
-  sgd = SGD(lr=0.1, decay=0, momentum=0, nesterov=False)
+  sgd = SGD(lr=1.1, decay=0, momentum=0, nesterov=False)
   model1.compile(loss='mse', optimizer=sgd)
 
-  model1.fit(X, y, nb_epoch=10, batch_size=10)
+  model1.fit(X, y, nb_epoch=150, batch_size=10)
   y_hat1 = model1.predict(X)
 
   pl.plot(y, '-^')
