2573de257e94e030e1bc66e081e36ca243199ad7
azure <alan.lee.azure@gmail.com>
2016-06-11 20:34:54 +0800

    turn off shuffle of keras.fit, spot-on match on sigmoid.

--- cmp_keras.py
+++ cmp_keras.py
@@ -8,15 +8,15 @@ import net, util
 
 if __name__ == '__main__':
   np.random.seed(1)
-  X = np.random.rand(80, 5)
-  y = np.sin(X.sum(axis=1)**3 + 1)[:,np.newaxis]
+  X = np.random.rand(20, 5)
+  y = (X.sum(axis=1)**3 + 1)[:,np.newaxis]
 
   model = net.Net([net.Dense(5,1), net.Sigmoid()])
 
   W = model.layers[0].W.copy()
   b = model.layers[0].b.copy()
 
-  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(1.1, 0), n_epoch=150, batch_size=10)
+  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(1.1, 0), n_epoch=5, batch_size=1)
   y_hat = model.predict(X)
 
   model1 = Sequential([
@@ -26,10 +26,12 @@ if __name__ == '__main__':
   sgd = SGD(lr=1.1, decay=0, momentum=0, nesterov=False)
   model1.compile(loss='mse', optimizer=sgd)
 
-  model1.fit(X, y, nb_epoch=150, batch_size=10)
+  model1.fit(X, y, nb_epoch=5, batch_size=1, shuffle=False)
   y_hat1 = model1.predict(X)
 
-  pl.plot(y, '-^')
+  print(np.allclose(y_hat, y_hat1))
+  print(model.layers[0].W, model.layers[0].b)
+  print(model1.get_weights())
   pl.plot(y_hat, '--o')
   pl.plot(y_hat1, '--*')
   pl.show()
