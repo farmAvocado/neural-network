0c0df268b689a2200e75547c68dc6acb6b2f3df8
azure <alan.lee.azure@gmail.com>
2016-06-11 21:22:28 +0800

    update.

--- cmp_keras.py
+++ cmp_keras.py
@@ -11,27 +11,39 @@ if __name__ == '__main__':
   X = np.random.rand(20, 5)
   y = (X.sum(axis=1)**3 + 1)[:,np.newaxis]
 
-  model = net.Net([net.Dense(5,1), net.Relu()])
+  model = net.Net([net.Dense(5,10), 
+                   net.Relu(),
+                   net.Dense(10,100),
+                   net.Sigmoid(),
+                   net.Dense(100,1),
+                   net.Linear()
+                 ])
 
   W = model.layers[0].W.copy()
   b = model.layers[0].b.copy()
+  W1 = model.layers[2].W.copy()
+  b1 = model.layers[2].b.copy()
+  W2 = model.layers[4].W.copy()
+  b2 = model.layers[4].b.copy()
 
-  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(0.1, 0), n_epoch=5, batch_size=10)
+  model.fit(X, y, loss=util.MSE(), optimizer=util.SGD(0.01, 0), n_epoch=5, batch_size=10)
   y_hat = model.predict(X)
 
   model1 = Sequential([
-      Dense(1, input_dim=5, weights=[W.T, b]),
-      Activation('relu')
+      Dense(10, input_dim=5, weights=[W.T, b]),
+      Activation('relu'),
+      Dense(100, weights=[W1.T, b1]),
+      Activation('sigmoid'),
+      Dense(1, weights=[W2.T, b2]),
+      Activation('linear')
     ])
-  sgd = SGD(lr=0.1, decay=0, momentum=0, nesterov=False)
+  sgd = SGD(lr=0.01, decay=0, momentum=0, nesterov=False)
   model1.compile(loss='mse', optimizer=sgd)
 
   model1.fit(X, y, nb_epoch=5, batch_size=10, shuffle=False)
   y_hat1 = model1.predict(X)
 
   print(np.allclose(y_hat, y_hat1))
-  print(model.layers[0].W, model.layers[0].b)
-  print(model1.get_weights())
   pl.plot(y_hat, '--o')
   pl.plot(y_hat1, '--*')
   pl.show()
