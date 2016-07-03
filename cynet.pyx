027795700bb2d3b911bef09de258ae75acdb410a
azure <alan.lee.azure@gmail.com>
2016-07-03 11:30:10 +0800

    add sgemv.

--- cynet.pyx
+++ cynet.pyx
@@ -4,7 +4,7 @@
 # cython: nonecheck=True
 
 import numpy as np
-from scipy.linalg.cython_blas cimport sgemm
+from scipy.linalg.cython_blas cimport sgemm, sgemv
 from libc.stdlib cimport malloc, free
 from libc.string cimport memset, memcpy
 
@@ -37,6 +37,36 @@ cpdef void rmo_sgemm(float_t[:,:] A, bint ta, float_t[:,:] B, bint tb, float_t[:
         &C[0,0], &ldc)
   return
 
+cpdef void rmo_sgemv(float_t[:,:] A, bint ta, float_t[:] x, float_t[:] y, float_t alpha=1.0, float_t beta=0.0) :
+  cdef:
+    char transa = 'N' if ta == 1 else 'T'
+    int m = A.shape[1]
+    int n = A.shape[0]
+    int lda = A.shape[1]
+    int incx = 1
+    int incy = 1
+
+  print(transa, m, n, lda, incx, incy)
+  sgemv(&transa,
+        &m, &n, &alpha,
+        &A[0,0], &lda,
+        &x[0], &incx,
+        &beta,
+        &y[0], &incy)
+  return
+
+cpdef void csgemv(char transa, int m, int n, float_t[:,:] A, int lda, float_t[:] x, int incx, float_t[:] y, int incy):
+  cdef:
+    float_t alpha = 1.0
+    float_t beta = 0.0
+
+  sgemv(&transa,
+        &m, &n, &alpha,
+        &A[0,0], &lda,
+        &x[0], &incx,
+        &beta,
+        &y[0], &incy)
+  return
 
 # initialization
 ############################################################
@@ -63,8 +93,8 @@ cdef class Dense:
   def __cinit__(self, n_in, n_out):
     self.W = init_uniform(0, 1.0, (n_out, n_in))
     self.b = np.zeros(n_out, dtype=FLOAT_T)
-    self.W_grad = np.zeros((n_out, n_in), dtype=FLOAT_T)
-    self.b_grad = np.zeros(n_out, dtype=FLOAT_T)
+    self.W_grad = np.empty_like(self.W, dtype=FLOAT_T)
+    self.b_grad = np.empty_like(self.b, dtype=FLOAT_T)
     self.inp = None
     self.outp = None
     self.n_in = n_in
@@ -93,4 +123,4 @@ cdef class Dense:
     rmo_sgemm(self.b_mult, 1, self.b, 0, self.outp, beta=1.0)
 
   cdef void backward(self, float_t[:,:] X):
-    pass
+    rmo_sgemm(X, 1, self.inp, 0, self.W_grad)
